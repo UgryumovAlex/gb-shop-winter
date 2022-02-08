b90798118133669bad77530c42d4d341f676fb09
ugryumov alexey <ugryumov_alex@mail.ru>
2022-02-08 23:15:15 +0300

    Все три модуля в рабочем состоянии

--- external-api/src/main/java/ru/gb/external/api/rest/ManufacturerController.java
+++ external-api/src/main/java/ru/gb/external/api/rest/ManufacturerController.java
@@ -1,12 +1,12 @@
 package ru.gb.external.api.rest;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.gb.api.manufacturer.api.ManufacturerGateway;
 import ru.gb.api.manufacturer.dto.ManufacturerDto;
+
 
 import java.util.List;
 
